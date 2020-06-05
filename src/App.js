2581dfda77a842e1bace0e8efaaf1c80311e702a
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-06-05 00:09:00 -0400

    file setup and logic for adding and removing from total

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coursesales from './Coursesales';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component{
 
-export default App;
+  render()
+  {
+      var courses = [
+        {name:'Complete IOS', price: 199},
+        {name:'Complete Python', price: 233},
+        {name:'Complete React', price: 250},
+        {name:'Complete Data Science', price: 299}
+        
+      ];
+      return(
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Course Sales</h2>
+          <Coursesales items={courses}/>
+        </header> 
+      </div>
+      )
+  }
+  
+  }
+
